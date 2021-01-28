22f948d603d373779134b7d10f66645a8f2488e0
Hector Delgado <Hector.d.delgado@gmail.com>
2021-01-28 13:38:27 -0800

    Added custom field names to DogBreed.kt

--- app/src/main/java/com/soundbite/packt/model/DogBreed.kt
+++ app/src/main/java/com/soundbite/packt/model/DogBreed.kt
@@ -1,15 +1,17 @@
 package com.soundbite.packt.model
 
+import com.squareup.moshi.Json
+
 data class DogBreed(
-    val bred_for: String? = null,
-    val breed_group: String? = null,
+    @Json(name = "bred_for")val bredFor: String? = null,
+    @Json(name = "breed_group")val breedGroup: String? = null,
     val height: Height,
     val id: Int,
     val image: Image? = null,
-    val life_span: String,
+    @Json(name = "life_span")val lifeSpan: String,
     val name: String,
     val origin: String? = null,
-    val reference_image_id: String,
+    @Json(name = "reference_image_id")val referenceImageId: String,
     val temperament: String? = null,
     val weight: Weight
 )
