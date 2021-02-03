9746556b80f4736680631d91b98076d212fad202
KoffeePunk <rfierro9389@gmail.com>
2021-02-02 19:40:31 -0800

    Progress on adding Drawer Navigation

--- app/src/main/java/com/soundbite/packt/ui/MainActivity.kt
+++ app/src/main/java/com/soundbite/packt/ui/MainActivity.kt
@@ -2,6 +2,11 @@ package com.soundbite.packt.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.navigation.NavigationView
+import com.soundbite.packt.R
 import com.soundbite.packt.databinding.ActivityMainBinding
 import com.soundbite.packt.domain.RetrofitServiceBuilder
 import com.soundbite.packt.network.DogApi
@@ -13,12 +18,24 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+//    private lateinit var drawerLayout: DrawerLayout
+//    private lateinit var navigationView: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+//        drawerLayout = binding.drawerLayout
+//        navigationView = binding.navView
+
+//        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+//        val navController = navHostFragment.navController
+
+//        NavigationUI.setupWithNavController(navigationView, navController)
+//        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
 
         // How to use DogApi
         // Get instance of service builder, pass in base url
